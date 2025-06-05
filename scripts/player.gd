3b8a36b1a3667731a22cd7c880e4437f1a4a1f00
coeyn <coeyn22@gmail.com>
2025-06-05 15:35:03 +0200

    Ajout des animations du personnages et blocage des déplacements en diagonales

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,9 +4,32 @@ const SPEED := 200.0
 
 func _physics_process(delta):
 	var direction = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"): direction.y -= 1
-	if Input.is_action_pressed("ui_down"): direction.y += 1
-	if Input.is_action_pressed("ui_left"): direction.x -= 1
-	if Input.is_action_pressed("ui_right"): direction.x += 1
-	velocity = direction.normalized() * SPEED
+
+	# Priorité verticale > horizontale
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		direction.x = 1
+
+	velocity = direction * SPEED
 	move_and_slide()
+
+	# Animation
+	var anim = ""
+	if direction.y > 0:
+		anim = "walk_down"
+	elif direction.y < 0:
+		anim = "walk_up"
+	elif direction.x > 0:
+		anim = "walk_right"
+	elif direction.x < 0:
+		anim = "walk_left"
+
+	if anim != "":
+		$AnimatedSprite2D.play(anim)
+	else:
+		$AnimatedSprite2D.stop()
